31795590bb2876338adb238fd07c94725c81042c
jsmith698 <85500280+jsmith698@users.noreply.github.com>
2021-10-15 22:50:48 -0500

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    console.log("Developer Notes");
+    //var el = document.getElementById("page name");
+    //el.innerText = "Developer Notes";
+      $("#page name").text("Developer Notes");
+  });
